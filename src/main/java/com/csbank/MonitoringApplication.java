5906beed52513223e47c6f5d53683e1664498480
Vincent Santarita <vsantari@lexmark.com>
2017-08-27 14:08:06 +0800

    Fixed code quality

--- src/main/java/com/csbank/MonitoringApplication.java
+++ src/main/java/com/csbank/MonitoringApplication.java
@@ -20,11 +20,17 @@ public class MonitoringApplication {
     private static final String COMPONENT_STATUS_GREEN = "Component Status: GREEN";
     private static final int CONNECTION_TIMEOUT = 3000;
     private static final long RESPONSE_TIME_THRESHOLD = 500;
+    private static final String MAP_STATUS_KEY = "status";
+    private static final String MAP_TIMESTAMP_KEY = "timestamp";
+    private static final String MAP_URL_KEY = "url";
+    private static final String MAP_RESPONSE_TIME_KEY = "responsetime";
+    private static final String MAP_ERROR_KEY = "error";
+
     private static final Logger LOG = LogManager.getLogger(MonitoringApplication.class);
 
     public static void main(String[] args) {
         LOG.debug("Start MonitoringApplication");
-        String urlStrings[] = null;
+        String[] urlStrings = null;
         String urlString = System.getProperty("URL_STRINGS");
         String urlStringPath = System.getProperty("URL_STRINGS_PATH");
 
@@ -36,7 +42,11 @@ public class MonitoringApplication {
 
         if (urlStrings != null) {
             ArrayList<LinkedHashMap<String, String>> results = startMonitor(urlStrings);
-            writeHtmlReport(results);
+            if (!results.isEmpty()) {
+                writeHtmlReport(results);
+            } else {
+                LOG.debug("Result is empty");
+            }
         } else {
             LOG.debug("Empty URL Strings");
         }
@@ -48,11 +58,12 @@ public class MonitoringApplication {
      * @return Array String
      */
     private static String[] readURLStringFromFile(String filename) {
-        String urlStrings[] = null;
+        String[] urlStrings = null;
         LOG.debug("Reading {}", filename);
+        BufferedReader bufferedReader = null;
         try {
             FileReader fileReader = new FileReader(filename);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            bufferedReader = new BufferedReader(fileReader);
             List<String> lines = new ArrayList<String>();
             String line = null;
             while ((line = bufferedReader.readLine()) != null) {
@@ -61,13 +72,16 @@ public class MonitoringApplication {
                     lines.add(line);
                 }
             }
-
-            bufferedReader.close();
             urlStrings = lines.toArray(new String[lines.size()]);
         } catch (IOException ie) {
             LOG.error("Failed to read {}. Found {}", filename, ie.toString());
+        }finally {
+            try {
+                bufferedReader.close();
+            } catch (IOException ie) {
+                LOG.error(ie.getMessage());
+            }
         }
-
        return urlStrings;
     }
 
@@ -77,7 +91,9 @@ public class MonitoringApplication {
      * @return ArrayList
      */
     private static  ArrayList<LinkedHashMap<String, String>> startMonitor(String[] urlStrings)  {
+        LOG.debug("Start startMonitor urlStrings={}", urlStrings.toString() );
         ArrayList<LinkedHashMap<String, String>> results = new ArrayList<LinkedHashMap<String, String>>();
+
         for (String urlString: urlStrings) {
             String error = null;
             boolean isGreenStatus = false;
@@ -104,12 +120,12 @@ public class MonitoringApplication {
                             if (isGreenStatus) {
                                 LinkedHashMap<String, String> output = new LinkedHashMap<String, String>();
                                 long responeTime = (end - start);
-                                output.put("timestamp", timestamp);
-                                output.put("status", "GREEN");
-                                output.put("url", urlString);
-                                output.put("responseTime", responeTime + " ms " + ((responeTime > RESPONSE_TIME_THRESHOLD) ? "(above threshold)" : "(below threshold)"));
-                                results.add(output);
+                                output.put(MAP_TIMESTAMP_KEY, timestamp);
+                                output.put(MAP_STATUS_KEY, "GREEN");
+                                output.put(MAP_URL_KEY, urlString);
+                                output.put(MAP_RESPONSE_TIME_KEY, responeTime + " ms " + ((responeTime > RESPONSE_TIME_THRESHOLD) ? "(above threshold)" : "(below threshold)"));
                                 LOG.debug("timestamp={}, status={}, url={}, responseTime={} ms", timestamp, "GREEN", urlString, (end - start) );
+                                results.add(output);
                             } else {
                                 error = "No \"" + COMPONENT_STATUS_GREEN + "\" strings from URL content";
                                 LOG.debug(error);
@@ -120,7 +136,6 @@ public class MonitoringApplication {
                         if (statusCode == HttpURLConnection.HTTP_OK) {
                             break;
                         }
-
                     }
                 } catch (IOException ioException) {
                     error = ioException.toString();
@@ -130,16 +145,17 @@ public class MonitoringApplication {
                         Thread.sleep(count * 1000);
                         LOG.error("{} Try connecting... Delaying for {} ms", count, count * 1000);
                     } catch (final InterruptedException ie) {
+                        LOG.error(ie.getMessage());
                     }
 
                 }
             }
             if (!isGreenStatus) {
                 LinkedHashMap<String, String> output = new LinkedHashMap<String, String>();
-                output.put("timestamp", timestamp);
-                output.put("status", "RED");
-                output.put("url", urlString);
-                output.put("error", error);
+                output.put(MAP_TIMESTAMP_KEY, timestamp);
+                output.put(MAP_STATUS_KEY, "RED");
+                output.put(MAP_URL_KEY, urlString);
+                output.put(MAP_ERROR_KEY, error);
                 results.add(output);
             }
         }
@@ -155,6 +171,7 @@ public class MonitoringApplication {
     private static String getUrlContents(InputStream inputStream) {
         StringBuilder content = new StringBuilder();
         try {
+            LOG.debug("Start getUrlContents");
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
             String line;
             while ((line = bufferedReader.readLine()) != null) {
@@ -163,7 +180,7 @@ public class MonitoringApplication {
             bufferedReader.close();
         }
         catch(Exception e) {
-            e.printStackTrace();
+            LOG.debug(e.getMessage());
         }
         return content.toString();
     }
@@ -174,6 +191,7 @@ public class MonitoringApplication {
      */
     private static void writeHtmlReport(ArrayList<LinkedHashMap<String, String>> results)  {
         try {
+            LOG.debug("Start writeHtmlReport");
             PrintWriter pw = new PrintWriter(new FileWriter("output.html"));
             pw.println("<h1> Success Results:</h1>");
             pw.println("<table border=1px; style=\"width:100%\">\n" + "  <tr> \n" + "    <th>Current timestamp</th>\n"
@@ -181,9 +199,9 @@ public class MonitoringApplication {
                     + RESPONSE_TIME_THRESHOLD + " ms)</th>\n" + "  </tr>");
             for(int i=0; i< results.size();i++)  {
                 pw.println("<tr>");
-                if (!results.get(i).keySet().contains("error")) {
+                if (!results.get(i).keySet().contains(MAP_ERROR_KEY)) {
                     for (String key : results.get(i).keySet()) {
-                        if (key == "status") {
+                        if (key == MAP_STATUS_KEY) {
                             pw.println("<td style=\"background-color: green\">" + results.get(i).get(key) + "</td>");
                         } else {
                             pw.println("<td>" + results.get(i).get(key) + "</td>");
@@ -199,9 +217,9 @@ public class MonitoringApplication {
                     + "    <th>Status check</th> \n" + "    <th>URL</th>\n" + "    <th>Error</th>\n" + "  </tr>");
             for(int i=0; i< results.size();i++)  {
                 pw.println("<tr>");
-                if (results.get(i).keySet().contains("error")) {
+                if (results.get(i).keySet().contains(MAP_ERROR_KEY)) {
                     for (String key : results.get(i).keySet()) {
-                        if (key == "status") {
+                        if (key == MAP_STATUS_KEY) {
                             pw.println("<td style=\"background-color: red\">" + results.get(i).get(key) + "</td>");
                         } else {
                             pw.println("<td>" + results.get(i).get(key) + "</td>");
@@ -213,7 +231,7 @@ public class MonitoringApplication {
             pw.println("</table>");
             pw.close();
         } catch (Exception ie) {
-            ie.printStackTrace();
+            LOG.debug(ie.getMessage());
         }
     }
 }
